f84de2628c988bd43a0117707ca7313b3927691c
NikitaLeosh <nikita_l90@live.ru>
2023-01-16 12:43:18 +0300

    Add project files.

---
+++ MekashronTest.Client/Pages/Index.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using IcuService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+
+namespace MekashronTest.Client.Pages
+{
+	public class IndexModel : PageModel
+	{
+		[BindProperty]
+		public string UserName { get; set; }
+		[BindProperty]
+		public string Password { get; set; }
+		[BindProperty]
+		public string? IPs { get; set; }
+
+		private LoginResponse response;
+		private JObject jResponse;
+		private LoginRequest request;
+		private readonly IICUTech _icuTech;
+		private readonly IHttpContextAccessor _accessor;
+		public IndexModel(IICUTech icuTech, IHttpContextAccessor accessor)
+		{
+			_icuTech = icuTech;
+			_accessor = accessor;
+		}
+		public void OnGet()
+		{
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			IPs = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+			Console.WriteLine(IPs);
+			request = new(UserName, Password, IPs);
+			response = await _icuTech.LoginAsync(request);
+			jResponse = JObject.Parse(response.@return);
+			if (response.@return.Contains("ResultCode", StringComparison.InvariantCultureIgnoreCase))
+			{
+				TempData["error"] = $"Error: {(string)jResponse["ResultMessage"]}";
+				Console.WriteLine(response.@return);
+				return Page();
+			}
+			string firstName = (string)jResponse["FirstName"];
+			string lastName = (string)jResponse["LastName"];
+			int entityId = (Int32)jResponse["EntityId"];
+			TempData["success"] = $"Success. User {firstName} {lastName} has logged in. Entity Id = {entityId}";
+			Console.WriteLine(JsonConvert.DeserializeObject(response.@return));
+			return Page();
+		}
+	}
+}
---
+++ MekashronTest.Client/Program.cs
@@ -0,0 +1,26 @@
+using IcuService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IICUTech, ICUTechClient>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
